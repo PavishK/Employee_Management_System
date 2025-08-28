e0c0c1b95681357135733e6314e3f97bef153056
PavishK <kpavish136@gmail.com>
2025-08-28 18:40:10 +0530

    EMS Version 1.0

--- ems_frontend/src/lib/config.js
+++ ems_frontend/src/lib/config.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "ems-website-ff25b.firebaseapp.com",
-  projectId: "ems-website-ff25b",
-  storageBucket: "ems-website-ff25b.firebasestorage.app",
-  messagingSenderId: "621324757286",
-  appId: "1:621324757286:web:be67fd2f53c1269b1b39e7",
-  measurementId: "G-FQSFEG1F52"
+  authDomain: "ems-website-da962.firebaseapp.com",
+  projectId: "ems-website-da962",
+  storageBucket: "ems-website-da962.firebasestorage.app",
+  messagingSenderId: "821870332976",
+  appId: "1:821870332976:web:e8aa4984516778d8a5f1f5",
+  measurementId: "G-Z6MQHTVYE8"
 };
 
 export const app = initializeApp(firebaseConfig);
